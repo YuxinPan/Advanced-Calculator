b387c2e7491d47bc7cd1e332e52017744f56cd03
Yuxin Pan <0@outlook.it>
2016-01-19 19:26:15 +0800

    Create compute.c

---
+++ compute.c
@@ -0,0 +1,28 @@
+//
+//  compute.c
+//  calculator
+//
+//  Created by Yuxin Pan on 15-8-27.
+//  Copyright (c) 2015 Yuxin Pan. All rights reserved.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "compute.h"
+#include <math.h>
+
+/* The inputs are two numbers and one operator. The output is the result of the operation. The function compute is used to simply add, subtract, multiply, divide and find the power of numbers */
+double compute(char str,double a,double b){
+    
+    char *symbl2 = "+-*/^";  // Five operators listed
+    
+    if (str==symbl2[0]){return (a+b);}
+    if (str==symbl2[1]){return (a-b);}
+    if (str==symbl2[2]){return (a*b);}
+    if (str==symbl2[3]){return (a/b);}
+    if (str==symbl2[4]){return pow(a,b);}
+    return 0;
+    
+}
