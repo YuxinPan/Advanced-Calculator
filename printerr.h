3254343198a9ffead0f3dfd2761894b0cf33818a
Yuxin Pan <0@outlook.it>
2016-01-19 19:29:10 +0800

    Create printerr.h

---
+++ printerr.h
@@ -0,0 +1,14 @@
+//
+//  printerr.h
+//  calculator2
+//
+//  Created by Yuxin Pan on 15-9-10.
+//  Copyright (c) 2015年 Yuxin Pan. All rights reserved.
+//
+
+#ifndef __calculator2__printerr__
+#define __calculator2__printerr__
+
+#include <stdio.h>
+
+#endif /* defined(__calculator2__printerr__) */
