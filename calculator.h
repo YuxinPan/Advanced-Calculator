6b6ff4a364820128bd758549bd10671a2a4b6235
Yuxin Pan <0@outlook.it>
2016-01-19 19:24:48 +0800

    Create calculator.h

---
+++ calculator.h
@@ -0,0 +1,14 @@
+//
+//  calculator.h
+//  calculator2
+//
+//  Created by Yuxin Pan on 15-8-27.
+//  Copyright (c) 2015 Yuxin Pan. All rights reserved.
+//
+ /* 
+#ifndef __calculator2__calculator__
+#define __calculator2__calculator__
+
+#include <stdio.h>
+
+#endifdefined(__calculator2__calculator__) */
