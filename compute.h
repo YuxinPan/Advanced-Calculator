8cec1ddb6f1365f10a6b289a1ada8c5896881639
Yuxin Pan <0@outlook.it>
2016-01-19 19:26:33 +0800

    Create compute.h

---
+++ compute.h
@@ -0,0 +1,14 @@
+//
+//  compute.h
+//  calculator
+//
+//  Created by Yuxin Pan on 15-8-27.
+//  Copyright (c) 2015 Yuxin Pan. All rights reserved.
+//
+
+#ifndef __calculator__compute__
+#define __calculator__compute__
+
+#include <stdio.h>
+
+#endif /* defined(__calculator__compute__) */
