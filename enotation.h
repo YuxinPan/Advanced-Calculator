d7309435d6681a4c93e8cd630bf959f892f47b65
Yuxin Pan <0@outlook.it>
2016-01-19 19:27:23 +0800

    Create enotation.h

---
+++ enotation.h
@@ -0,0 +1,14 @@
+//
+//  enotation.h
+//  calculator2
+//
+//  Created by 潘宇忻 on 15-9-12.
+//  Copyright (c) 2015年 Yuxin Pan. All rights reserved.
+//
+
+#ifndef __calculator2__enotation__
+#define __calculator2__enotation__
+
+#include <stdio.h>
+
+#endif /* defined(__calculator2__enotation__) */
