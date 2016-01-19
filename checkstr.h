a46064f0154a71337eefef0aded91a66bbaeba94
Yuxin Pan <0@outlook.it>
2016-01-19 19:25:51 +0800

    Create checkstr.h

---
+++ checkstr.h
@@ -0,0 +1,14 @@
+//
+//  checkstr.h
+//  calculator
+//
+//  Created by Yuxin Pan on 15-8-27.
+//  Copyright (c) 2015 Yuxin Pan. All rights reserved.
+//
+
+#ifndef __calculator__checkstr__
+#define __calculator__checkstr__
+
+#include <stdio.h>
+
+#endif /* defined(__calculator__checkstr__) */
